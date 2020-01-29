791e6e00c5e3732a27cd2cc93dff74b201328cd2
Andrea <andreademarco91@gmail.com>
2020-01-29 15:09:55 +0100

    Esercizio 3 OK consollog

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ESERCIZIO 1
 // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
 var studente = {
   'nome' : 'Andrea',
@@ -7,15 +8,53 @@ var studente = {
   'livello' : 'beginner'
 }
 
-console.log(studente);
+console.log('Esercizio 1', studente);
 
-for (var proprieta in studente) {
-  console.log(studente[proprieta]);
+for (var key in studente) {
+  proprieta = (studente[key]);
 }
 
+// ESERCIZIO 2
+// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome.
 
-Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome.
+var studenti = [
+  {
+    'nome' : 'Michele',
+    'cognome' : 'Alberti',
+    'eta' : 28,
+    'sesso' : 'm',
+    'livello' : 'beginner'
+  },
+  {
+    'nome' : 'Antonio',
+    'cognome' : 'Giano',
+    'eta' : 24,
+    'sesso' : 'm',
+    'livello' : 'beginner'
+  },
+  {
+    'nome' : 'Savio',
+    'cognome' : 'Nicolai',
+    'eta' : 35,
+    'sesso' : 'm',
+    'livello' : 'beginner'
+  }
+]
 
+for (var i = 0; i < studenti.length; i++) {
+  var alunno = studenti[i]
+  console.log('Esercizio 2', alunno);
+  console.log(alunno.nome + ' ' + alunno.cognome);
+}
 
+// ESERCIZIO 3
+// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
-Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+var iscritto = {
+  'nome' : prompt('inserisci il tuo nome'),
+  'cognome' : prompt('inserisci il tuo Cognome'),
+  'eta' : parseInt(prompt('inserisci la tua eta')),
+}
+studenti.push(iscritto)
+
+console.log('Esercizio 3', studenti);
