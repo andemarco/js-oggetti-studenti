32d105c0bc6720e3625c911481d94d5036d8c078
Andrea <andreademarco91@gmail.com>
2020-01-29 16:28:10 +0100

    W/T HANDLEBARS OK

--- js/script.js
+++ js/script.js
@@ -1,67 +1,81 @@
-// ESERCIZIO 1
+$(document).ready(function () {// ESERCIZIO 1
 // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
-var studente = {
+  var studente = {
+    'Nome' : 'Andrea',
+    'Cognome' : 'De Marco',
+    'Eta' : 28,
+    'Sesso' : 'm',
+    'Livello' : 'beginner'
+  }
+
+  console.log('Esercizio 1', studente);
+
+  for (var key in studente) {
+    var proprieta = key + ':' + studente[key] + ', ';
+    console.log(proprieta);
+    $('.es1 .student').append(proprieta)
+  }
+
+// METODO handlebars
+var source = document.getElementById("entry-template").innerHTML;
+var template = Handlebars.compile(source);
+var context = {
   'Nome' : 'Andrea',
   'Cognome' : 'De Marco',
   'Eta' : 28,
   'Sesso' : 'm',
   'Livello' : 'beginner'
-}
+};
+var html = template(context);
+$('.es1_handlebars').append(html)
+  // ESERCIZIO 2
+  // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome.
 
-console.log('Esercizio 1', studente);
+  var studenti = [
+    {
+      'nome' : 'Michele',
+      'cognome' : 'Alberti',
+      'eta' : 28,
+      'sesso' : 'm',
+      'livello' : 'beginner'
+    },
+    {
+      'nome' : 'Antonio',
+      'cognome' : 'Giano',
+      'eta' : 24,
+      'sesso' : 'm',
+      'livello' : 'beginner'
+    },
+    {
+      'nome' : 'Savio',
+      'cognome' : 'Nicolai',
+      'eta' : 35,
+      'sesso' : 'm',
+      'livello' : 'beginner'
+    }
+  ]
 
-for (var key in studente) {
-  var proprieta = key + ':' + studente[key] + ', ';
-  console.log(proprieta);
-  $('.es1 .student').append(proprieta)
-}
-// ESERCIZIO 2
-// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome.
-
-var studenti = [
-  {
-    'nome' : 'Michele',
-    'cognome' : 'Alberti',
-    'eta' : 28,
-    'sesso' : 'm',
-    'livello' : 'beginner'
-  },
-  {
-    'nome' : 'Antonio',
-    'cognome' : 'Giano',
-    'eta' : 24,
-    'sesso' : 'm',
-    'livello' : 'beginner'
-  },
-  {
-    'nome' : 'Savio',
-    'cognome' : 'Nicolai',
-    'eta' : 35,
-    'sesso' : 'm',
-    'livello' : 'beginner'
+  for (var i = 0; i < studenti.length; i++) {
+    var alunno = studenti[i]
+    console.log('Esercizio 2', alunno);
+    console.log(alunno.nome + ' ' + alunno.cognome);
+    $('.es2 .student').append(alunno.nome + ' ' + alunno.cognome + ', ')
   }
-]
 
-for (var i = 0; i < studenti.length; i++) {
-  var alunno = studenti[i]
-  console.log('Esercizio 2', alunno);
-  console.log(alunno.nome + ' ' + alunno.cognome);
-  $('.es2 .student').append(alunno.nome + ' ' + alunno.cognome + ', ')
-}
+  // ESERCIZIO 3
+  // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+  //
+  var iscritto = {
+    'nome' : prompt('inserisci il tuo nome'),
+    'cognome' : prompt('inserisci il tuo Cognome'),
+    'eta' : parseInt(prompt('inserisci la tua eta')),
+  }
+  studenti.push(iscritto)
 
-// ESERCIZIO 3
-// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
-//
-var iscritto = {
-  'nome' : prompt('inserisci il tuo nome'),
-  'cognome' : prompt('inserisci il tuo Cognome'),
-  'eta' : parseInt(prompt('inserisci la tua eta')),
-}
-studenti.push(iscritto)
+  console.log('Esercizio 3', studenti);
 
-console.log('Esercizio 3', studenti);
-
-for (var i = 0; i < studenti.length; i++) {
-  var alunno = studenti[i]
-  $('.es3 .student').append(studenti[i].nome + ' ' + studenti[i].cognome + ' ' + studenti[i].eta + ', ')
-}
+  for (var i = 0; i < studenti.length; i++) {
+    var alunno = studenti[i]
+    $('.es3 .student').append(studenti[i].nome + ' ' + studenti[i].cognome + ' ' + studenti[i].eta + ', ')
+  }
+});
